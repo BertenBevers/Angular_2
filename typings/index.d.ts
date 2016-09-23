ab9e42c37cfbf8cd27992264a7fce236a97c8a0a
bertenbevers <berten.bevers@telenet.be>
2016-09-23 14:23:11 +0200

    fix: added selenium-webdriver typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="globals/angular-protractor/index.d.ts" />
 /// <reference path="globals/core-js/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/selenium-webdriver/index.d.ts" />
 /// <reference path="custom-typings.d.ts" />
